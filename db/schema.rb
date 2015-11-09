8fadda9adbe6c88501beb735a34390616914ebcc
“jiemu” <gemlareinarotol@gmail.com>
2015-11-09 11:49:02 +0800

    push to github

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106030106) do
+ActiveRecord::Schema.define(version: 20151108045206) do
 
-  create_table "todo_lists", force: :cascade do |t|
+  create_table "todo_items", force: true do |t|
+    t.integer  "todo_list_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "completed_at"
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+
+  create_table "todo_lists", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
---
+++ spec/features/todo_items/edit_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Editing todo items" do 
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.")}
+  let!(:todo_item) { todo_list.todo_items.create(content: "Milk") }
+
+  it "is successful with valid content" do
+	visit_todo_list(todo_list)
+	within("#todo_item_#{todo_item.id}") do
+	  click_link "Edit"
+	end
+	fill_in "Content", with: "Lots of Milk"
+	click_button "Save"
+	expect(page).to have_content("Saved todo list item.")
+	todo_item.reload #to make sure title is Lots of Milk
+	expect(todo_item.content).to eq("Lots of Milk")
+  end
+
+  it "is unsuccessful with no content" do
+	visit_todo_list(todo_list)
+	within("#todo_item_#{todo_item.id}") do
+	  click_link "Edit"
+	end
+	fill_in "Content", with: ""
+	click_button "Save"
+	expect(page).to_not have_content("Saved todo list item.")
+	expect(page).to have_content("Content can't be blank")
+	todo_item.reload #to make sure title is Lots of Milk
+	expect(todo_item.content).to eq("Milk")
+  end
+
+   it "is unsuccessful with not enought content" do
+	visit_todo_list(todo_list)
+	within("#todo_item_#{todo_item.id}") do
+	  click_link "Edit"
+	end
+	fill_in "Content", with: "H"
+	click_button "Save"
+	expect(page).to_not have_content("Saved todo list item.")
+	expect(page).to have_content("Content is too short")
+	todo_item.reload #to make sure title is Lots of Milk
+	expect(todo_item.content).to eq("Milk")
+  end
+
+end
